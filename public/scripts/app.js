1067ef43acc0037c9dcc826972204d9f8ee1ddc7
Frank <frankzou2000@hotmail.com>
2019-10-18 22:57:40 +0000

    test

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+
 $(() => {
   $.ajax({
     method: "GET",
@@ -8,3 +10,17 @@ $(() => {
     }
   });;
 });
+
+
+const $searchValue = $(".searchvalue");
+  $searchValue.on('submit', function (event) {
+    event.preventDefault();
+    fetch(`http://www.omdbapi.com/?t=${$searchValue.val()}&apikey=7f07f227`)
+      .then(res => res.json())
+        .then((out) => {
+          console.log('Output: ', out);
+    }).catch(err => console.error(err));
+    })
+
+
+  });
